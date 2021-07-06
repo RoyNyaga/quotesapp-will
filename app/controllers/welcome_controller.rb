af6b926895bf286b24cf0da13f6c85e489d5e675
RoyNyaga <nyagaandreroy@gmail.com>
2021-07-06 01:20:45 +0100

    create welcome create action

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,16 @@
 class WelcomeController < ApplicationController
   def index
+    @quote = Quote.new
   end
+
+  def create_quote 
+    Quote.create(quote_params)
+    redirect_to root_path
+  end
+
+  private 
+
+  def quote_params
+    params.require(:quote).permit(:content, :author_name)
+  end  
 end
