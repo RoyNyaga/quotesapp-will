94861fa2d150b59e79ea86bebf8e4425d1cad92c
RoyNyaga <nyagaandreroy@gmail.com>
2021-07-06 01:23:04 +0100

    refactor welcome index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root to: "welcome#index"
+  post '/create-quote', to: 'welcome#create_quote', as: 'create_quote'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
