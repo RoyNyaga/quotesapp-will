e0756fd41ba7aad1978bd92583da1edf7230b64d
RoyNyaga <nyagaandreroy@gmail.com>
2021-07-14 16:35:28 +0100

    add api controller routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   root to: "welcome#index"
   post '/create-quote', to: 'welcome#create_quote', as: 'create_quote'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      get '/quotes', to: 'quotes#index', as: 'quotes'
+    end
+  end
 end
